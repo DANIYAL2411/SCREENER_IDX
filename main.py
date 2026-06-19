e40e57e7e8a01cb13ebcdd360481a24430958636
DANIYAL2411 <danialoppo144@gmail.com>
2026-06-19 16:19:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import requests
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-msg = "✅ Screener IDX Online"
-
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-requests.post(url, data={
-    "chat_id": CHAT_ID,
-    "text": msg
-})
+r = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "✅ Screener IDX Online"
+    }
+)
 
-print("Message sent")
+print(r.status_code)
+print(r.text)
